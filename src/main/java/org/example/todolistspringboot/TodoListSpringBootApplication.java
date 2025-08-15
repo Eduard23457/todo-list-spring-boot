f3809ec29b7450c2d395b548603812037a0354bd
Эдуард <khidiyatov2000@bk.ru>
2025-08-15 18:49:07 +0500

    Initial commit

---
+++ src/main/java/org/example/todolistspringboot/TodoListSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.todolistspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListSpringBootApplication.class, args);
+    }
+
+}
