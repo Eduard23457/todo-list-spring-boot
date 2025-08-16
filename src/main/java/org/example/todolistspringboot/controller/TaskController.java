61f22ee5a5499991254be52178f7cf5502ac2da4
Эдуард <khidiyatov2000@bk.ru>
2025-08-16 18:14:17 +0500

    Add swager

--- src/main/java/org/example/todolistspringboot/controller/TaskController.java
+++ src/main/java/org/example/todolistspringboot/controller/TaskController.java
@@ -1,6 +1,8 @@
 package org.example.todolistspringboot.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.todolistspringboot.dto.TaskDtoRequest;
 import org.example.todolistspringboot.dto.TaskDtoResponse;
@@ -13,34 +15,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
+@Tag(name = "Task API", description = "CRUD операции с задачами")
 public class TaskController {
 
     private final TaskService taskService; // final поле, Lombok создаст конструктор
-
+    @Operation(summary = "Создать новую задачу")
     @PostMapping("/add")
     public ResponseEntity<TaskDtoResponse> createTask(@RequestBody TaskDtoRequest taskDtoRequest) {
         TaskDtoResponse response = taskService.createTask(taskDtoRequest);
         return ResponseEntity.ok(response);
     }
-
+    @Operation(summary = "Получить список всех задач")
     @GetMapping("/findAll")
     public ResponseEntity<List<TaskDtoResponse>> getAllTask() {
         List<TaskDtoResponse> allTask = taskService.findAllTask();
         return ResponseEntity.ok(allTask);
     }
-
+    @Operation(summary = "получить задачу по id")
     @GetMapping("/findById")
     public ResponseEntity<TaskDtoResponse> findById(@RequestParam Long id) {
         TaskDtoResponse task = taskService.findById(id);
         return ResponseEntity.ok(task);
     }
-
+    @Operation(summary = "обновить задачу по id")
     @PutMapping("/update")
     public ResponseEntity<TaskDtoResponse> updateTask(@RequestParam Long id,
                                                       @RequestBody TaskDtoRequest taskDtoRequest) {
         TaskDtoResponse taskDtoResponse = taskService.updateEntity(id, taskDtoRequest);
         return ResponseEntity.ok(taskDtoResponse);
     }
+    @Operation(summary = "удалить задачу по id")
     @DeleteMapping("/delete")
     public ResponseEntity<Void>deleteById(@RequestParam Long id){
         taskService.deleteById(id);
--- src/main/java/org/example/todolistspringboot/service/TaskService.java
+++ src/main/java/org/example/todolistspringboot/service/TaskService.java
@@ -2,6 +2,7 @@ package org.example.todolistspringboot.service;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.todolistspringboot.dto.TaskDtoRequest;
 import org.example.todolistspringboot.dto.TaskDtoResponse;
 import org.example.todolistspringboot.entity.Task;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 
@@ -22,22 +23,28 @@ public class TaskService {
     private final TaskMapper taskMapper;
 
     public TaskDtoResponse createTask(@Valid TaskDtoRequest taskDtoRequest) {
+        log.info("Добавление задачи в базу данных {}", taskDtoRequest);
         Task task = taskMapper.toEntity(taskDtoRequest);
         Task response = taskRepository.save(task);
+        log.info("задача добавлена {}", response);
         return taskMapper.toDto(response);
 
     }
 
     public List<TaskDtoResponse> findAllTask() {
+        log.info("Вывод всех задач из базы данных");
         List<Task> allTask = taskRepository.findAll();
         List<TaskDtoResponse> listTask = taskMapper.toList(allTask);
+        log.info("все задачи {}", listTask);
         return listTask;
 
     }
 
     public TaskDtoResponse findById(Long id) {
+        log.info("Вывод задачи по id{}",id);
         Task task = taskRepository.findById(id).orElseThrow(()
                 -> new RuntimeException("not found id in bd" + id));
+        log.info("Найдена задача с id {}: {}", id, task);
         return taskMapper.toDto(task);
     }
 
@@ -46,14 +53,18 @@ public class TaskService {
                 -> new RuntimeException("not found id in bd" + id));
         taskMapper.update(taskDtoRequest, task);
         Task savedTask = taskRepository.save(task);
+        log.info("Задача с id {} обновлена: {}", id, savedTask);
+
         return taskMapper.toDto(savedTask);
     }
 
     public void deleteById(Long id) {
         if (!taskRepository.existsById(id)) {
             throw new TaskNotFoundException(id);
+
         }
         taskRepository.deleteById(id);
+        log.info("Задача с id {} удалена", id);
     }
 
 }
